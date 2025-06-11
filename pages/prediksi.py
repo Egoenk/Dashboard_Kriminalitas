4bfd7e097cd728726da3625e213015459936b675
Egy <neoegoenk@gmail.com>
2025-06-11 14:02:28 +0700

    adssa

--- pages/prediksi.py
+++
@@ -1,9 +0,0 @@
-import dash
-from dash import html
-
-dash.register_page(__name__, path="/prediksi", name="Prediksi")
-
-layout = html.Div([
-    html.H2("Halaman Prediksi"),
-    html.P("Ini adalah halaman prediksi.")
-])
